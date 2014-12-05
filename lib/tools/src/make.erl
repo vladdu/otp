016b1df98d756c28070ea96fd02001da6b45ffae
Vlad Dumitrescu <vladdu55@gmail.com>
2014-12-05 17:25:50 +0100

    Merge ct_make.erl in make.erl
    These modules are almost identical. The ct_make behaviour (other return
    values) is enabled by using the option 'return_details'-

--- lib/tools/src/make.erl
+++ lib/tools/src/make.erl
@@ -1,26 +1,26 @@
 %%
 %% %CopyrightBegin%
-%% 
-%% Copyright Ericsson AB 1996-2013. All Rights Reserved.
-%% 
+%%
+%% Copyright Ericsson AB 1996-2014. All Rights Reserved.
+%%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
 %% compliance with the License. You should have received a copy of the
 %% Erlang Public License along with this software. If not, it can be
 %% retrieved online at http://www.erlang.org/.
-%% 
+%%
 %% Software distributed under the License is distributed on an "AS IS"
 %% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 %% the License for the specific language governing rights and limitations
 %% under the License.
-%% 
+%%
 %% %CopyrightEnd%
 %%
 %% Purpose : Basic make facility
 
 %% Compares date stamps of .erl and Object files - recompiles when
 %% necessary.
-%% Files to be checked are contained in a file 'Emakefile' 
+%% Files to be checked are contained in a file 'Emakefile'
 %% If Emakefile is missing the current directory is used.
 -module(make).
 
@@ -28,7 +28,7 @@
 
 -include_lib("kernel/include/file.hrl").
 
--define(MakeOpts,[noexec,load,netload,noload]).
+-define(MakeOpts,[noexec,load,netload,noload,return_details]).
 
 all() ->
     all([]).
@@ -39,7 +39,7 @@ all(Options) ->
 	Files when is_list(Files) ->
 	    do_make_files(Files,MakeOpts);
 	error ->
-	    error
+	    {error,[]}
     end.
 
 files(Fs) ->
@@ -50,12 +50,13 @@ files(Fs0, Options) ->
     {MakeOpts,CompileOpts} = sort_options(Options,[],[]),
     case get_opts_from_emakefile(Fs,'Emakefile',CompileOpts) of
 	Files when is_list(Files) ->
-	    do_make_files(Files,MakeOpts);	    
-	error -> error
+	    do_make_files(Files,MakeOpts);
+	error ->
+	    {error,[]}
     end.
 
 do_make_files(Fs, Opts) ->
-    process(Fs, lists:member(noexec, Opts), load_opt(Opts)).
+    process(Fs, lists:member(noexec, Opts), lists:member(return_details, Opts), load_opt(Opts), []).
 
 
 sort_options([H|T],Make,Comp) ->
@@ -85,7 +86,7 @@ read_emakefile(Emakefile,Opts) ->
 	{ok,Emake} ->
 	    transform(Emake,Opts,[],[]);
 	{error,enoent} ->
-	    %% No Emakefile found - return all modules in current 
+	    %% No Emakefile found - return all modules in current
 	    %% directory and the options given at command line
 	    Mods = [filename:rootname(F) ||  F <- filelib:wildcard("*.erl")],
 	    [{Mods, Opts}];
@@ -96,14 +97,14 @@ read_emakefile(Emakefile,Opts) ->
 
 transform([{Mod,ModOpts}|Emake],Opts,Files,Already) ->
     case expand(Mod,Already) of
-	[] -> 
+	[] ->
 	    transform(Emake,Opts,Files,Already);
-	Mods -> 
+	Mods ->
 	    transform(Emake,Opts,[{Mods,ModOpts++Opts}|Files],Mods++Already)
     end;
 transform([Mod|Emake],Opts,Files,Already) ->
     case expand(Mod,Already) of
-	[] -> 
+	[] ->
 	    transform(Emake,Opts,Files,Already);
 	Mods ->
 	    transform(Emake,Opts,[{Mods,Opts}|Files],Mods++Already)
@@ -117,8 +118,8 @@ expand(Mods,Already) when is_list(Mods), not is_integer(hd(Mods)) ->
     lists:concat([expand(Mod,Already) || Mod <- Mods]);
 expand(Mod,Already) ->
     case lists:member($*,Mod) of
-	true -> 
-	    Fun = fun(F,Acc) -> 
+	true ->
+	    Fun = fun(F,Acc) ->
 			  M = filename:rootname(F),
 			  case lists:member(M,Already) of
 			      true -> Acc;
@@ -134,14 +135,14 @@ expand(Mod,Already) ->
 	    end
     end.
 
-%%% Reads the given Emakefile to see if there are any specific compile 
+%%% Reads the given Emakefile to see if there are any specific compile
 %%% options given for the modules.
 get_opts_from_emakefile(Mods,Emakefile,Opts) ->
     case file:consult(Emakefile) of
 	{ok,Emake} ->
 	    Modsandopts = transform(Emake,Opts,[],[]),
 	    ModStrings = [coerce_2_list(M) || M <- Mods],
-	    get_opts_from_emakefile2(Modsandopts,ModStrings,Opts,[]); 
+	    get_opts_from_emakefile2(Modsandopts,ModStrings,Opts,[]);
 	{error,enoent} ->
 	    [{Mods, Opts}];
 	{error,Other} ->
@@ -151,7 +152,7 @@ get_opts_from_emakefile(Mods,Emakefile,Opts) ->
 
 get_opts_from_emakefile2([{MakefileMods,O}|Rest],Mods,Opts,Result) ->
     case members(Mods,MakefileMods,[],Mods) of
-	{[],_} -> 
+	{[],_} ->
 	    get_opts_from_emakefile2(Rest,Mods,Opts,Result);
 	{I,RestOfMods} ->
 	    get_opts_from_emakefile2(Rest,RestOfMods,Opts,[{I,O}|Result])
@@ -160,7 +161,7 @@ get_opts_from_emakefile2([],[],_Opts,Result) ->
     Result;
 get_opts_from_emakefile2([],RestOfMods,Opts,Result) ->
     [{RestOfMods,Opts}|Result].
-    
+
 members([H|T],MakefileMods,I,Rest) ->
     case lists:member(H,MakefileMods) of
 	true ->
@@ -172,14 +173,14 @@ members([],_MakefileMods,I,Rest) ->
     {I,Rest}.
 
 
-%% Any flags that are not recognixed as make flags are passed directly
+%% Any flags that are not recognised as make flags are passed directly
 %% to the compiler.
 %% So for example make:all([load,debug_info]) will make everything
 %% with the debug_info flag and load it.
-	
+
 load_opt(Opts) ->
     case lists:member(netload,Opts) of
-	true -> 
+	true ->
 	    netload;
 	false ->
 	    case lists:member(load,Opts) of
@@ -191,17 +192,35 @@ load_opt(Opts) ->
     end.
 
 
-process([{[],_Opts}|Rest], NoExec, Load) ->
-    process(Rest, NoExec, Load);
-process([{[H|T],Opts}|Rest], NoExec, Load) ->
+process([{[],_Opts}|Rest], NoExec, Details, Load, Result) ->
+    process(Rest, NoExec, Details, Load, Result);
+process([{[H|T],Opts}|Rest], NoExec, Details, Load, Result) ->
     case recompilep(coerce_2_list(H), NoExec, Load, Opts) of
 	error ->
-	    error;
-	_ ->
-	    process([{T,Opts}|Rest], NoExec, Load)
+	    process([{T,Opts}|Rest], NoExec, Details, Load, [{H,error}|Result]);
+	Info ->
+	    process([{T,Opts}|Rest], NoExec, Details, Load, [{H,Info}|Result])
     end;
-process([], _NoExec, _Load) ->
-    up_to_date.
+process([], NoExec, Details, _Load, Result) ->
+    if not NoExec ->
+	    case lists:keysearch(error, 2, Result) of
+		{value,_} ->
+	    case Details of
+			true -> {error,Result};
+			false -> error
+		end;
+		false ->
+	    case Details of
+			true ->  {up_to_date,Result};
+			false -> up_to_date
+		end
+	    end;
+       true ->
+	    case Details of
+			true -> Result;
+			false -> up_to_date
+		end
+    end.
 
 recompilep(File, NoExec, Load, Opts) ->
     ObjName = lists:append(filename:basename(File),
@@ -218,11 +237,16 @@ recompilep(File, NoExec, Load, Opts) ->
 	false ->
 	    recompile(File, NoExec, Load, Opts)
     end.
- 
+
 recompilep1(File, NoExec, Load, Opts, ObjFile) ->
     {ok, Erl} = file:read_file_info(lists:append(File, ".erl")),
     {ok, Obj} = file:read_file_info(ObjFile),
-	 recompilep1(Erl, Obj, File, NoExec, Load, Opts).
+    case {readable(Erl), writable(Obj)} of
+	{true, true} ->
+	    recompilep1(Erl, Obj, File, NoExec, Load, Opts);
+	_ ->
+	    error
+    end.
 
 recompilep1(#file_info{mtime=Te},
 	    #file_info{mtime=To}, File, NoExec, Load, Opts) when Te>To ->
@@ -238,7 +262,7 @@ recompile2(ObjMTime, File, NoExec, Load, Opts) ->
 	true ->
 	    recompile(File, NoExec, Load, Opts);
 	false ->
-	    false
+	    up_to_date
     end.
 
 include_opt([{i,Path}|Rest]) ->
@@ -252,15 +276,21 @@ include_opt([]) ->
 %% Actually recompile and load the file, depending on the flags.
 %% Where load can be netload | load | noload
 
-recompile(File, true, _Load, _Opts) ->
-    io:format("Out of date: ~ts\n",[File]);
-recompile(File, false, noload, Opts) ->
+recompile(File, NoExec, Load, Opts) ->
+    case do_recompile(File, NoExec, Load, Opts) of
+	{ok,_} -> ok;
+	Other -> Other
+    end.
+
+do_recompile(_File, true, _Load, _Opts) ->
+    out_of_date;
+do_recompile(File, false, noload, Opts) ->
     io:format("Recompile: ~ts\n",[File]),
     compile:file(File, [report_errors, report_warnings, error_summary |Opts]);
-recompile(File, false, load, Opts) ->
+do_recompile(File, false, load, Opts) ->
     io:format("Recompile: ~ts\n",[File]),
     c:c(File, Opts);
-recompile(File, false, netload, Opts) ->
+do_recompile(File, false, netload, Opts) ->
     io:format("Recompile: ~ts\n",[File]),
     c:nc(File, Opts).
 
@@ -272,6 +302,14 @@ exists(File) ->
 	    false
     end.
 
+readable(#file_info{access=read_write}) -> true;
+readable(#file_info{access=read})       -> true;
+readable(_) -> false.
+
+writable(#file_info{access=read_write}) -> true;
+writable(#file_info{access=write})      -> true;
+writable(_) -> false.
+
 coerce_2_list(X) when is_atom(X) ->
     atom_to_list(X);
 coerce_2_list(X) ->
@@ -281,14 +319,14 @@ coerce_2_list(X) ->
 %%% time larger than the modification time of the object
 %%% file, return true. Otherwise return false.
 check_includes(File, IncludePath, ObjMTime) ->
-    Path = [filename:dirname(File)|IncludePath], 
+    Path = [filename:dirname(File)|IncludePath],
     case epp:open(File, Path, []) of
 	{ok, Epp} ->
 	    check_includes2(Epp, File, ObjMTime);
 	_Error ->
 	    false
     end.
-    
+
 check_includes2(Epp, File, ObjMTime) ->
     case epp:parse_erl_form(Epp) of
 	{ok, {attribute, 1, file, {File, 1}}} ->
